4691614ec7d2482435da456e18824111ee5a34a7
Raider37 <56130875+Raider37@users.noreply.github.com>
2020-12-17 22:10:07 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,14 @@
+let burger = "";
+document.addEventListener("DOMContentLoaded", function() {
+  burger = document.getElementById('burger')
+  burger.onclick = toggleBurger
+});
+function toggleBurger() {
+  if(burger.classList.contains("active")){
+    burger.classList.remove("active");
+  }else{
+    burger.classList += " active";
+  }
+}
+
+
